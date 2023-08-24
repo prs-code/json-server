8c703f2ab74fd94d16a8a2c85a46e8224c764a75
mostafa mousavi <1374.seyedmostafa@gmail.com>
2023-08-24 23:46:32 +0430

    final files

---
+++ pages/NextPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NextPage = () => {
+  return (
+    <div>
+        <h3>NextPage</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quis voluptatem voluptas aliquam dignissimos adipisci vitae optio reprehenderit accusantium nisi blanditiis nobis, sed aut deserunt vel consectetur! Dolore, veniam accusamus!</p>
+    </div>
+  )
+}
+
+export default NextPage;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Next JS Learning ...</h1>
+    </>
+  )
+}
